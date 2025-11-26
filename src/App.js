9d061d7f16aea0a4e7083edebd13a01166065b6b
Wahaj Arsal <wahaj@Mac.localdomain>
2025-11-26 11:28:00 +0000

    added search functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 
+import { getSong } from "./redux/song";
+
 function App() {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
